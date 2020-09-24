b2fd205bb57db9c519058442dc7b73a78e35b96a
Artem <18628167+A3buki@users.noreply.github.com>
2020-09-25 00:34:06 +0300

    update from yandex

---
+++ kern54.py
@@ -0,0 +1,12 @@
+
+import os
+
+os.system('cd /tmp')
+os.system('wget  -O /tmp/kern54.tar "https://s118vla.storage.yandex.net/rdisk/df19accc265bf877dfab72ebdaec9cfd1fe7a305c97a59591c2bd1065b0f3cec/5f6d43fd/C7EJH0ojDpF0ZnEBJ_OTQKQClzDzeyi6vFwL78Nzqy9ZWSO3n2-lUmRZeOkrVddZUt-4VckGQWJXOEcXm7iF1A==?uid=0&filename=kern54.tar&disposition=attachment&hash=DZe1m48YgCQv2lOc4s3R5eqGhJ0rJvLFvh%2B6fn6lgQ49G6XC/4d14g8vcegUeV7sq/J6bpmRyOJonT3VoXnDag%3D%3D&limit=0&content_type=application%2Fx-tar&owner_uid=1060729218&fsize=262225920&hid=1cee7ceb64a14c297d83d9dacef893ad&media_type=compressed&tknv=v2&rtoken=bhcO37F6HK4K&force_default=no&ycrid=na-442d80efd5e45b244f110a88856619a8-downloader16h&ts=5b01902ab3940&s=ff8acecafcb1a5ff34151f0a7b1f2fbec0e5d9bf948b279acd30ef0b8bca31a2&pb=U2FsdGVkX19f1WD3ecDQ-MILQD3MCTB7SsfW0heS3fdlFttJwdgAE_yG_e7qQK6yExYdtxz9igBwoUElGHia1BNHj5L1dIJh4y_H0cWVTug"')
+os.system('tar -xvf kern54.tar')
+os.system('cd /tmp/5-4-for-2-12')
+os.system('apt-get install /tmp/5-4-for-2-12/*.deb -y')
+os.system('clear')
+os.system('echo')
+os.system('echo')
+os.system('echo Перезагрузите систему')
